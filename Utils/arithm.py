dbff6ee6ae93ec96d2c52030764261af12ae2e0f
FrancoisdeFouchecour <francois.de-salivet-de-fouchecour@polytechnique.edu>
2019-01-30 14:39:29 +0100

    Utils: add ppcm function

--- Utils/arithm.py
+++ Utils/arithm.py
@@ -19,4 +19,29 @@ def prime_decomposition(n):
         return([n])
     else:
         return([factor]+prime_decomposition(int(n/factor)))
+
+
+def prime_decomposition_normalise(decompostion):
+    #reformat prime decomposition
+    unique_decomposition = []
+    for num in decompostion:
+        if unique_decomposition.count(num) == 0:
+            unique_decomposition.append(num)
+    
+    return([(num, decompostion.count(num))for num in unique_decomposition])
+        
+
+def ppcm_multiple(tab_number):
+    #return the ppcm of all numbers in tab
+    tab_decomposition = [prime_decomposition_normalise(prime_decomposition(num)) for num in tab_number]
+    ppcm_decomposition = []
+    
+    for decomposition in tab_decomposition:
+        for (div, exp) in decomposition:
+            if exp > ppcm_decomposition.count(div):
+                for i in range(exp-ppcm_decomposition.count(div)):
+                    ppcm_decomposition.append(div)
+    
+    return ppcm_decomposition.sort()
+                    
         
