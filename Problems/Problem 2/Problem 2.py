1c8dbb954072112eee5c274b5173e8634efbba99
FrancoisdeFouchecour <francois.de-salivet-de-fouchecour@polytechnique.edu>
2019-01-30 12:43:55 +0100

    Add problem 2

---
+++ Problems/Problem 2/Problem 2.py
@@ -0,0 +1,63 @@
+import time 
+
+
+problem_number = 2
+test_input = 100
+test_solution = 2 + 8 + 34 
+problem_input = 4000000
+
+
+#Solution
+
+
+def solution(limit):
+    current = 2
+    previous = 1
+    count = 0
+
+    while(previous < limit):
+        if(previous%2 == 0):
+            count += previous
+        current, previous = (current + previous, current)
+        
+    return count
+
+
+#Test & Result
+
+
+fichier = open("Solution "+str(problem_number)+".txt", "w")
+string = ""
+
+begin_test = time.time()
+test_value = solution(test_input)
+end_test = time.time()
+test_time = end_test - begin_test
+
+string += "TEST #1\n\n"
+string += "Input: "+str(test_input)+"\n"
+string += "Output: "+str(test_value)+"\n"
+string += "Answer: "+str(test_solution)+"\n"
+string += "Computation time: "+str(test_time)+" sec\n"
+string += "Verification: "
+
+if(test_value == test_solution):
+    string += "TRUE"
+else:
+    string += "FALSE"
+    
+
+begin_problem = time.time()
+problem_value = solution(problem_input)
+end_problem = time.time()
+problem_time = end_problem - begin_problem
+
+string += "\n\n\nRESULT PROBLEM #"+str(problem_number)+"\n\n"
+string += "Input: "+str(problem_input)+"\n"
+string += "Output: "+str(problem_value)+"\n"
+string += "Computation time: "+str(problem_time)+" sec\n"
+
+string += "\n\n\nCurrent date & time: " + time.strftime("%c")
+
+fichier.write(string)
+fichier.close()
