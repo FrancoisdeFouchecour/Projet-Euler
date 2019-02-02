89aae1a092a40e72a6b2ea311fcd991dade76ada
FrancoisdeFouchecour <francois.de-salivet-de-fouchecour@polytechnique.edu>
2019-02-02 16:29:32 +0100

    Utils/algorithm: add two_sum_index

--- Utils/algorithm.py
+++ Utils/algorithm.py
@@ -15,4 +15,25 @@ def two_sum(Set, target):
         elif x == target:
             return(True)
     return(False)
+
+
+def two_sum_index(Set, target):
+    #Return index of two value of Set sum to value (Set is ordered)
+    size = len(Set)
+    if size == 0:
+        return []
+    i = 0
+    j = size - 1
+    x = Set[i] + Set[j]
+    
+    while i < j:
+        if x < target:
+            i += 1
+            x = Set[i] + Set[j]
+        elif x > target:
+            j -= 1
+            x = Set[i] + Set[j]
+        elif x == target:
+            return([[Set[i], Set[j]]] + two_sum_index(Set[i + 1:j], target))
+    return([])
     
