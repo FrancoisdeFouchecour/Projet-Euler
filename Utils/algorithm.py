d65e686f22ec59b595c271e1c327921a6f12c321
FrancoisdeFouchecour <francois.de-salivet-de-fouchecour@polytechnique.edu>
2019-02-03 16:54:34 +0100

    Utils/algorithm: add quick add and quick search

--- Utils/algorithm.py
+++ Utils/algorithm.py
@@ -36,4 +36,44 @@ def two_sum_index(Set, target):
         elif x == target:
             return([[Set[i], Set[j]]] + two_sum_index(Set[i + 1:j], target))
     return([])
+
     
+def quick_search(Set, x):
+    if len(Set) == 0:
+        return(False)
+    elif len(Set) == 1:
+        return(Set[0] == x)
+    else:
+        min_index = 0
+        max_index = len(Set) - 1
+        while min_index <= max_index:
+            new_index = (max_index + min_index)//2
+            if Set[new_index] == x:
+                return True
+            elif Set[new_index] < x:
+                min_index = new_index + 1
+            else:
+                max_index = new_index - 1
+        return(False)
+
+
+def quick_add(Set, x):
+    if len(Set) == 0:
+        return(0)
+    elif Set[0] > x:
+        return(0)
+    elif Set[-1] < x:
+        return(len(Set))
+    else:
+        min_index = 0
+        max_index = len(Set) - 1
+        while min_index <= max_index:
+            new_index = (max_index + min_index)//2
+            if Set[new_index] == x:
+                return new_index
+            elif Set[new_index] < x:
+                min_index = new_index + 1
+            else:
+                max_index = new_index - 1
+        return(min_index)
+    return(0)
