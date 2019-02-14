ec53ccdf2f0fa294e1398a9e23f00333d4263a68
FrancoisdeFouchecour <francois.de-salivet-de-fouchecour@polytechnique.edu>
2019-02-14 18:56:33 +0100

    Revert "Add Problem 701"
    This reverts commit a3f2a18fdc606e85f5bcbd4d3ae9e5503c66e0eb.

--- Problems/70-Problem/Problem 70.py
+++ Problems/70-Problem/Problem 70.py
@@ -1,10 +1,9 @@
 import time 
-import numpy as np
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.getcwd())),'Utils'))
-from arithm import min_factor
-from algorithm import is_permutation
+from arithm import gcd
+
 
 problem_number = 70
 problem_input = 10000000
@@ -14,29 +13,8 @@ problem_input = 10000000
 
 
 def solution(limit):
-    primes = min_factor(limit - 1)
-    phi_tab = [0 for _ in range(limit)]
     
-    phi_tab[1] = 1
-    for n in range(2, limit):
-        k = primes[n]
-        if k == n:
-            phi_tab[n] = n - 1
-        elif n%(k*k) == 0:
-            phi_tab[n] = k*phi_tab[n//k]
-        else:
-            phi_tab[n] = (k - 1)*phi_tab[n//k]
-    
-    
-    min_value = np.infty
-    min_index = -1
-    
-    for n in range(2, limit):
-        if n/phi_tab[n] < min_value and is_permutation(str(n), str(phi_tab[n])):
-            min_value = n/phi_tab[n]
-            min_index = n
-    
-    return min_index
+    return 0
 
 
 #Test & Result
